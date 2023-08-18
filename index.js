1bf74272ad8942e79d3dda18c389fa8f8ffa4baa
Alex Trotman <alextrotman04@gmail.com>
2023-08-18 15:30:02 -0500

    commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+// function superbowlWin(array){
+//     array.find(array.result === "W")
+// }
+
+// const found = array.find((element) => element === "W");
+
+function superbowlWin(array){
+    const object = array.find((element) => element.result === "W");
+    if (object){
+        return object.year
+    }
+}
